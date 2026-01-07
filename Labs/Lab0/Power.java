e2f0d1fa6709b9e0fa3a4a7d58d839c8bfde1035
Collins Cao <YuqiCao99@gmail.com>
2026-01-07 15:37:04 -0800

    Complete Lab0 programs

---
+++ Labs/Lab0/Power.java
@@ -0,0 +1,13 @@
+public class Power {
+    public static void main(String[] args) {
+        long result = 1L << 31;
+        System.out.println(result);
+    }
+
+    /*
+     * Differences between Java and Python:
+     * 1. Java requires explicit type declarations, Python does not.
+     * 2. Java code must be inside a class, Python code does not.
+     * 3. Java needs a main method to run, Python runs top-down.
+     */
+}
